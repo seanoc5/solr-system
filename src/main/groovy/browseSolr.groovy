dc48440052b6a8dcc0c3d51d39bdb196fea0aa1a
Sean O'Connor <seanoc5@gmail.com>
2022-08-18 23:58:56 -0400

    scripts to process docs from solr

---
+++ src/main/groovy/browseSolr.groovy
@@ -0,0 +1,32 @@
+/**
+ * @author :    sean
+ * @mailto :    seanoc5@gmail.com
+ * @created :   8/3/22, Wednesday
+ * @description:
+ */
+
+
+import org.apache.log4j.Logger
+import org.apache.solr.client.solrj.SolrClient
+import org.apache.solr.client.solrj.SolrQuery
+import org.apache.solr.client.solrj.impl.HttpSolrClient
+import org.apache.solr.client.solrj.response.QueryResponse
+import org.apache.solr.common.SolrDocumentList
+
+final Logger log = Logger.getLogger(this.class.name);
+
+log.info "Starting ${this.class.name}..."
+
+String host = 'localhost'
+int port = 8983
+String collection = 'solr_system'
+String baseUrl = "http://$host:$port/solr/$collection"
+SolrClient solrClient = new HttpSolrClient.Builder(baseUrl).build()
+SolrQuery sq = new SolrQuery("*:*")
+QueryResponse resp = solrClient.query(sq)
+SolrDocumentList sdl = resp.getResults()
+sdl.each { sdoc ->
+    log.info "Doc: ${sdoc.id}"
+}
+
+log.info "Done...?"
