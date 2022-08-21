70aeea2f530a2b770a5d6f0b86995645abc091a8
seanoc5 <seanoc5@gmail.com>
2022-08-21 15:16:45 -0400

    add comments

--- src/main/resources/configs/configLocate.groovy
+++ src/main/resources/configs/configLocate.groovy
@@ -2,6 +2,7 @@ package configs
 
 solrUrl = "http://localhost:8983/solr/solr_system"
 
+// todo -- these name: path entries are not yet used, still reading path and name from cli args
 dataSources {
     localFiles {
         MyDesktop: '/home/sean/Desktop'
@@ -10,6 +11,9 @@ dataSources {
     }
 }
 
+// note: all of the labels below are optional/customizable. You probably want to leave the `ignore`, `index`, and `analyze` labels as is, they have special meaning
+
+// edit and customize these entries. The namePattern name is the assigned tag, the regex pattern (`~` is the pattern operator) are the matching regexes for the label/tag
 files.namePatterns = [
         ignore      : ~/([.~]*lock.*|_.*|.*\.te?mp$|.*\.class$|robots.txt)/,
         index       : ~/(bash.*|csv|groovy|ics|ipynb|java|lst|md|php|py|rdf|rss|scala|sh|tab|te?xt|tsv)/,
@@ -21,12 +25,13 @@ files.namePatterns = [
         control     : ~/.*(ignore|license.*|lock|pem)/,
         data        : ~/.*(csv|jsonl?d?|lst|pbix|tab|tsv)/,
         media       : ~/.*(avi|jpe?g|ogg|mp3|mpe?g|png|wav)/,
-        logs        : ~/.*(logs?\.?\d*)/,
-        archive     : ~/.*(arc|gz|parquet|rar|zip|tar.gz|zip)/,
+        logs        : ~/.*(logs?\.?\d*)/,       // attempting to allow singular or plural (option s after log, and 0 or more digits)
+        archive     : ~/.*(arc|gz|parquet|rar|tar.gz|zip)/,
         web         : ~/.*(html?)/,
         system      : ~/.*(_SUCCESS|bat|bin|bkup|cache|class|cookies|deb|gcc|lib|\.old|pkg|rpm|#$)/,
 ]
 
+// folder names can be matches to assign tags for a given folder (name matching)
 folders.namePatterns = [
         ignore : ~/(__snapshots__|\.bsp|\.cache|\.csv|\.gradle|\.git|\.github|\.idea|\.settings|\.svn|\.vscode|_global|ignore.*|runtime)/,
         content: ~/(content)/,
