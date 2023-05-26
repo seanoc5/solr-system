c12ca30a0f817dfa6c07e75e6d73a736fced9d9c
seanoc5@gmail.com <sean.oconnor@lucidworks.com>
2023-05-25 21:42:50 -0400

    add ArchFile/Folder model (rename?)
    add methods to get solr input docs from archve

--- src/main/groovy/com/oconeco/helpers/ArchiveUtils.groovy
+++ src/main/groovy/com/oconeco/helpers/ArchiveUtils.groovy
@@ -1,5 +1,8 @@
 package com.oconeco.helpers
 
+
+import com.oconeco.models.ArchFile
+import com.oconeco.models.ArchFolder
 import org.apache.commons.compress.archivers.ArchiveEntry
 import org.apache.commons.compress.archivers.ArchiveInputStream
 import org.apache.commons.compress.archivers.tar.TarArchiveInputStream
@@ -8,8 +11,11 @@ import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream
 import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream
 import org.apache.commons.io.FilenameUtils
 import org.apache.log4j.Logger
+import org.apache.solr.common.SolrInputDocument
 
 import java.nio.file.Files
+import java.util.regex.Pattern
+
 /**
  * @author :    sean
  * @mailto :    seanoc5@gmail.com
@@ -31,18 +37,25 @@ class ArchiveUtils {
         if (name.endsWith('tar.gz') || name.endsWith('tgz')) {
             inputStream = new GzipCompressorInputStream(archiveFile.newInputStream());
             ais = new TarArchiveInputStream(inputStream)
-        } else if (fileType.equalsIgnoreCase('application/zip') || name.endsWith('zip') ) {
+        } else if (fileType.equalsIgnoreCase('application/zip') || name.endsWith('zip')) {
             ais = new ZipArchiveInputStream(archiveFile.newInputStream());
-        } else if (name.endsWith('bz2') ) {
-                inputStream = new BZip2CompressorInputStream(archiveFile.newInputStream());
-                ais = new TarArchiveInputStream(inputStream)
+        } else if (name.endsWith('bz2')) {
+            inputStream = new BZip2CompressorInputStream(archiveFile.newInputStream());
+            ais = new TarArchiveInputStream(inputStream)
         } else {
             log.warn "Unknown archive type: $archiveFile"
         }
         return ais
     }
 
-    static gatherArchiveEntries(ArchiveInputStream ais) {
+    static gatherArchiveEntries(File f) {
+        ArchiveInputStream aiszip = ArchiveUtils.getArchiveInputStream(f)
+        List<ArchiveEntry> archiveEntries = gatherArchiveEntries(aiszip)
+        return archiveEntries
+    }
+
+
+    static List<ArchiveEntry> gatherArchiveEntries(ArchiveInputStream ais) {
         List<ArchiveEntry> entries = []
         ArchiveEntry entry = null
         while ((entry = ais.getNextEntry()) != null) {
@@ -62,10 +75,43 @@ class ArchiveUtils {
     }
 
 
-    static boolean isArchiveFileByExtension(File file){
+    static List<SolrInputDocument> gatherSolrInputDocs(File f, String locationName = Constants.LBL_UNKNOWN, String crawlName = Constants.LBL_UNKNOWN, Integer depth = null) {
+        ArchiveInputStream aiszip = ArchiveUtils.getArchiveInputStream(f)
+        List<SolrInputDocument> sidList = gatherSolrInputDocs(aiszip, locationName, crawlName, depth)
+        return sidList
+    }
+
+    static List<SolrInputDocument> gatherSolrInputDocs(ArchiveInputStream ais, String locationName = Constants.LBL_UNKNOWN, String crawlName = Constants.LBL_UNKNOWN, Integer depth = null) {
+        List<SolrInputDocument> sidList = []
+        ArchiveEntry entry = null
+        while ((entry = ais.getNextEntry()) != null) {
+            if (entry.isDirectory()) {
+                ArchFolder folder = new ArchFolder(entry, locationName, crawlName, depth)
+                SolrInputDocument sid = folder.toSolrInputDocument()
+                sidList << sid
+                log.info "Archive folder (solr doc): ${sid})"
+            } else {
+                log.info "\t\tarchive entry file (${entry.name}) -- type:(${entry.class.simpleName})"
+                ArchFile file = new ArchFile(entry, locationName, crawlName, depth)
+                SolrInputDocument sid = file.toSolrInputDocument()
+                sidList << sid
+//                Long size = entry.size
+//                sidList << entry
+                log.debug "\t\tArchive File (solr doc): ${sid})"
+
+            }
+        }
+        return sidList
+    }
+
+
+    static final Pattern ARCHIVE_EXTENSIONS = Pattern.compile('.*(tar.gz|tgz|tar.z|tar.bz2?|zip)$')
+
+    static boolean isArchiveFileByExtension(File file) {
         String name = file.name.toLowerCase()
         boolean archive = false
-        if(name.endsWith('tar.gz') || name.endsWith('tgz') || name.endsWith('tar.z') || name.endsWith('tar.bz2')){
+//        if(name.endsWith('tar.gz') || name.endsWith('tgz') || name.endsWith('tar.z') || name.endsWith('tar.bz2')){
+        if (name ==~ ARCHIVE_EXTENSIONS) {
             archive = true
         }
 
