7ad7311e28ad0c2e4b21e2a1ad3108b969008859
Sean O'Connor <seanoc5@gmail.com>
2023-05-28 17:25:37 -0400

    change arg from 'name' to 'locationName' for more clarity (?)

--- src/main/groovy/com/oconeco/helpers/SolrCrawlArgParser.groovy
+++ src/main/groovy/com/oconeco/helpers/SolrCrawlArgParser.groovy
@@ -30,9 +30,9 @@ class SolrCrawlArgParser {
         CliBuilder cli = new CliBuilder(usage: "${toolName}.groovy --config=configLocate.groovy", width: 160)
         cli.with {
             h longOpt: 'help', 'Show usage information'
-            c longOpt: 'config', args: 1, required: true, argName: 'ConfigFile', 'optional configuration file to be parsed/slurped with configSlurper'
-            f longOpt: 'folders', 'Parent folder(s) to crawl' , args: '+', valueSeparator: ',', required: false
-            n longOpt: 'name', args:1, required: false, 'Name of crawl to be added to all solr input docs (files and folders)'
+            c longOpt: 'config', required: true, args: 1, argName: 'ConfigFile', 'optional configuration file to be parsed/slurped with configSlurper'
+            f longOpt: 'folders', required: false, 'Parent folder(s) to crawl' , args: '+', valueSeparator: ','
+            l longOpt: 'locationName', args:1, required: false, 'Name of source location to be added to all solr input docs (files and folders), i.e. hostname, profile name, imap account,...'
             s longOpt: 'solrUrl', args: 1, required: false, 'Solr url with protocol, host, and port (if any)'
             w longOpt: 'wipeContent', required: false, 'Clear data from previous crawl (beware--easy to really bork things with this..)'
         }
@@ -46,57 +46,44 @@ class SolrCrawlArgParser {
         }
 
         String configLocation = options.config
-        File cfgFile = new File(configLocation)
+        log.info "Config location: $configLocation"
+        URL cfgUrl = SolrCrawlArgParser.class.getClassLoader().getResource('./configs/configLocate.groovy')
+//        def cfgUrl = SolrCrawlArgParser.class.getResource(configLocation)     // todo -- remember that static methods need the getClassLoader() call
+        File cfgFile = new File(cfgUrl.toURI())
 
-        ClassLoader cl = Thread.currentThread().getContextClassLoader();
         ConfigObject config = null
-
-        def resourcesRoot = cl.getResource('.')
-        log.info "\t\tStarting with resources root: $resourcesRoot"
         if (cfgFile.exists()) {
             log.info "\t\tcfg file: ${cfgFile.absolutePath}"
+            config = new ConfigSlurper().parse(cfgUrl)
         } else {
-            log.info "\t\tcould not find config file: ${cfgFile.absolutePath}, trying resource path: $resourcesRoot"
-            URL cfgUrl = cl.getResource(configLocation)
-            if (cfgUrl) {
-                cfgFile = new File(cfgUrl.toURI())
-                if (cfgFile.exists()) {
-                    log.info "\t\tFound config file (${configLocation}) in resources: ${cfgFile.absolutePath}"
-                    config = new ConfigSlurper().parse(cfgFile.toURI().toURL())
-                } else {
-                    throw new IllegalArgumentException("Config file: $cfgFile does not exist, bailing...")
-                }
-            } else {
-                log.warn "No config url found?? configLocation:$configLocation "
-                throw new IllegalArgumentException("No config url found in args, exiting!!!")
-            }
+            log.warn "\t\tcould not find config file: ${cfgFile.absolutePath}"
+//            URL cfgUrl = cl.getResource(configLocation)
         }
 
 
         if (options.solrUrl) {
             log.info "\t\tUsing Solr url from Command line (overriding config file): ${options.solrUrl}"
             config.solrUrl = options.solrUrl
-        } else if(config.solrUrl){
+        } else if(config?.solrUrl){
             log.info "\t\tUsing Solr url from CONFIG file): ${config.solrUrl}"
         } else {
             log.info "\t\tNo solr url given in command line args NOR config file, this should not be possible, exiting..."
             System.exit(-2)
         }
 
-        def localFolders = config.dataSources.localFolders
-        def folders = options.folderss
+        def localFolders = config?.dataSources.localFolders
         if(options.folderss){
             List<String> ds = options.folderss
             int cnt = ds.size()
             Map dsMap = ds.collectEntries{it.split(':')}
             log.info "\t\tReplace config folders ($localFolders) with folders from command line args: ($dsMap) "
-            config.dataSources.localFolders = dsMap
+            config?.dataSources.localFolders = dsMap
         }
 
         def wipe = options.wipeContent
         if(wipe){
             log.warn "Found flag `wipeContent` which will send deletes for each datasource"
-            config.wipeContent = true
+            config?.wipeContent = true
         } else if(config.wipeContent){
             log.warn "Found config file setting `wipeContent` with value (${config.wipeContent}) which might send deletes for each datasource"
         }
