8b2ee158ab39abf6962d88c63c75b4431638c0f8
Sean O'Connor <seanoc5@gmail.com>
2023-06-03 23:27:08 -0400

    move crawler into crawlmap loop add placeholder for wipeContent processing
    add folder.addFolderDetails() call
    
    move buildChildrenList call
    
    minor reporting of solr UpdateResponse results

--- src/main/groovy/misc/crawlFileSystem.groovy
+++ src/main/groovy/misc/crawlFileSystem.groovy
@@ -3,13 +3,13 @@ package misc
 import com.oconeco.analysis.FileAnalyzer
 import com.oconeco.analysis.FolderAnalyzer
 import com.oconeco.crawler.LocalFileSystemCrawler
-import com.oconeco.helpers.Constants
 import com.oconeco.helpers.SolrCrawlArgParser
 import com.oconeco.models.FSFolder
+import com.oconeco.models.SavableObject
 import com.oconeco.persistence.SolrSaver
 import org.apache.log4j.Logger
+import org.apache.solr.client.solrj.response.UpdateResponse
 
-import java.nio.file.LinkOption
 import java.util.regex.Pattern
 
 Logger log = Logger.getLogger(this.class.name);
@@ -17,33 +17,43 @@ log.info "Start ${this.class.name}, with args: $args"
 
 ConfigObject config = SolrCrawlArgParser.parse(this.class.simpleName, args)
 
-Map<String,String> crawlMap = config.dataSources.localFolders
-log.info "\t\tCrawl Map (start folders): $crawlMap"
+Map<String, String> crawlMap = config.dataSources.localFolders
+log.info "\t\tCrawl Map (config.dataSources.localFolders): $crawlMap"
 
 String solrUrl = config.solrUrl
-log.info "\t\tSolr url: $solrUrl"
+log.debug "\t\tSolr url: $solrUrl"
 
 String locationName = config.sourceName ?: 'undefined'
-String crawlName = config.ccrawlName ?: 'undefined'
 
-//Map folderNamePatterns = config.namePatterns.folders
-Pattern fileIgnorePattern = config.namePatterns.folders.ignore
+Pattern fileIgnorePattern = config.namePatterns.files.ignore
+Pattern folderIgnorePattern = config.namePatterns.folders.ignore
 
-SolrSaver solrSaver = new SolrSaver(solrUrl, locationName)
+SolrSaver solrSaver = new SolrSaver(solrUrl)
 log.info "\t\tSolr Saver created: $solrSaver"
 
 FolderAnalyzer folderAnalyzer = new FolderAnalyzer(config)
 FileAnalyzer fileAnalyzer = new FileAnalyzer(config)
 
-def linkOption = LinkOption.NOFOLLOW_LINKS
-LocalFileSystemCrawler crawler = new LocalFileSystemCrawler(locationName, crawlName)
 long start = System.currentTimeMillis()
 
-crawlMap.each { String label, String startPath ->
-    log.info "crawl map item with label:'$label' --- and --- startpath:'$startPath'..."
-     def crawlFolders = crawler.buildCrawlFolders(startPath, fileIgnorePattern)
-    crawlFolders.each { FSFolder fsfolder->
-        log.info "\t\tCrawl folder: $fsfolder"
+crawlMap.each { String crawlName, String startPath ->
+    LocalFileSystemCrawler crawler = new LocalFileSystemCrawler(locationName, crawlName)
+    if(config.wipeContent==true){
+//        crawler.de
+        log.warn "Config/args indicates we whould wipe content for crawl: $crawler"
+    }
+    log.info "\t\tcrawl map item with label:'$crawlName' --- and --- startpath:'$startPath'..."
+    def crawlFolders = crawler.buildCrawlFolders(startPath, folderIgnorePattern)
+    crawlFolders.each { FSFolder fsfolder ->
+        def details = fsfolder.addFolderDetails()
+        log.debug "\t\t$fsfolder: added folder details: $details"
+
+        List<SavableObject> children = fsfolder.buildChildrenList()
+        // todo -- add analysis call here, to update folder with (basic) info of children
+        def sidList = fsfolder.toSolrInputDocumentList()
+        UpdateResponse response = solrSaver.saveDocs(sidList)
+        log.info "\t\tCrawled folder: $fsfolder with ${fsfolder.children.size()} children -- Solr update response (status:${response.getStatus()} -- ${response.getElapsedTime()}ms): $response)"
+//        log.info "\t\tSolr update response: $response"
     }
 }
 
@@ -51,7 +61,7 @@ solrSaver.closeClient()
 
 long end = System.currentTimeMillis()
 long elapsed = end - start
-log.info "${this.class.name} Elapsed time: ${elapsed}ms (${elapsed/1000} sec)"
+log.info "${this.class.name} Elapsed time: ${elapsed}ms (${elapsed / 1000} sec)"
 
 //crawlMap.each { String label, String startPath ->
 //    Path path = Paths.get(startPath)
