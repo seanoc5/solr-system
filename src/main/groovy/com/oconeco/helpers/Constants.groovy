e19eaac72f742c225adf731e807b6bae93be907e
Sean O'Connor <seanoc5@gmail.com>
2023-06-03 22:47:54 -0400

    update constants
    trying to update case-insensitive flags in patterns (useful???)

--- src/main/groovy/com/oconeco/helpers/Constants.groovy
+++ src/main/groovy/com/oconeco/helpers/Constants.groovy
@@ -10,27 +10,27 @@ class Constants {
     public static final String LBL_UNKNOWN = 'unknown'
 
     public static final Map<String, Pattern> DEFAULT_FOLDERNAME_PATTERNS = [
-            ignore       : ~/.*(\.gradle|\.m2|\b*snapshots?\b*|\b*caches?\b*|git|github|ignore.*|node_modules|packages?|pkgs?|plugins?|repository|skins?|skipme.*|svn|target|tmp|vscode)/,
-            backups      : ~/.*(backups?|bkups?|old|timeshift)/,
-            configuration: ~/.*(configs)/,
+            ignore       : ~/.*(?i)(csv|\.gradle|\.m2|\b*snapshots?\b*|\b*caches?\b*|deleteme.*|git|github|ignore.*|node_modules|packages?|pkgs?|plugins?|repository|skins?|skipme.*|svn|target|tmp|vscode)/,
+            backups      : ~/.*(?i)(backups?|bkups?|old|timeshift)/,
+            configuration: ~/.*(?i)(configs)/,
             documents    : ~/.*([Dd]ocuments|[Dd]esktop)/,
             downloads    : ~/.*([Dd]ownloads)/,
             pictures     : ~/.*([Pp]ictures)/,
-            programming  : ~/.*(java|jupyter|src|work)/,
-            system       : ~/.*(class|sbt|sbt-\d.*|runtime)/,
+            programming  : ~/.*(?i)(java|jupyter|src|work)/,
+            system       : ~/.*(?i)(class|sbt|sbt-\d.*|runtime)/,
     ]
 
     public static final Map<String, Pattern> DEFAULT_FILENAME_PATTERNS = [
-            ignore      : ~/([.~]*lock.*|_.*|.*\.te?mp$|.*\.class$|.*\.pem|robots.txt|skipme.*|.*\.tmp)/,
-            office      : ~/.*\.(accdb|docx?|ods|odp|odt|pptx?|rtf|vsdx?|xlsx?)/,
-            system      : ~/.*(\.(bin|deb|lib|pkg|rpm)|(gcc.*))/,
-            archive     : ~/.*\.(arc|gz|rar|zip|tar.gz|zip)/,
-            web         : ~/.*(html?)/,
+            ignore      : ~/(?i)([.~]*lock.*|_.*|.*\bte?mp|.*\.class|.*\.pem|skipme.*)/,
+            office      : ~/.*\.(?i)(accdb|docx?|ods|odp|odt|pptx?|rtf|vsdx?|xlsx?)/,
+            system      : ~/.*(\.(?i)(bin|deb|lib|pkg|rpm)|(gcc.*))/,
+            archive     : ~/.*\.(?i)(arc|gz|rar|zip|tar.gz|zip)/,
+            web         : ~/.*(?i)(html?)/,
             instructions: ~'(?i).*(adoc|readme.*|md)',
-            techDev     : ~/.*(c|css|go|groovy|gradle|jar|java|javascript|js|php|schema|sh)/,
-            config      : ~/.*(cfg|config.*|pem|properties|xml|yaml)/,
-            data        : ~/.*(csv|jsonl?d?|lst|tab)/,
-            media       : ~/.*(avi|jpe?g|ogg|mp3|mpe?g|wav)/,
+            techDev     : ~/.*(?i)(c|css|go|groovy|gradle|jar|java|javascript|js|php|schema|sh)/,
+            config      : ~/.*(?i)(cfg|config.*|pem|properties|xml|yaml)/,
+            data        : ~/.*(?i)(csv|jsonl?d?|lst|tab)/,
+            media       : ~/.*(?i)(avi|jpe?g|ogg|mp3|mpe?g|wav)/,
     ]
     public static int DEFAULT_FOLDER_SIZE = 4096     // todo -- revisit, this is just an arbitrary starting size...?
 
