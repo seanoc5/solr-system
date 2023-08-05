e9f22ff2c35def944e3368aa020e4c7c0c33ddb1
Sean O'Connor <seanoc5@gmail.com>
2023-08-05 16:48:05 -0400

    refactoring Firefox base object

--- src/main/groovy/com/oconeco/models/FFObject.groovy
+++ src/main/groovy/com/oconeco/models/FFObject.groovy
@@ -1,8 +1,9 @@
 package com.oconeco.models
 
-
+import com.oconeco.persistence.SolrSystemClient
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.core.Logger
+import org.apache.solr.common.SolrInputDocument
 
 /**
  * @author :    sean
@@ -21,18 +22,25 @@ class FFObject extends SavableObject {
     public static final int TYPE_BOOKMARK_CODE = 1
     public static final int TYPE_FOLDER_CODE = 2
     public static final int TYPE_SEPARATOR_CODE = 3
+    // todo -- are there more codes? I think we just ignore anything not 1 or 2 at the moment...
     public static final int MICROS_LEN = '1686926500000000'.length()
-    public static final String TYPE = 'FFPObject'
+    //semi-random microsecond value from an actual bookmark/entry -- ignore the value, just using the magnitude
+    public static final String TYPE = 'FFObject'
 
     /** firefox id for tracking/comparison & will concat with type to create SolrSystem id */
     String ffId = ''
-    String uri = ''
-    def children = []
+    /** FF bookmark "global" id... some are definately NOT global, but the important ones seem to be */
+    String guid
+    /** probably just an order int for display */
+    Integer idx = null
 
     FFObject(def object, SavableObject parent, String locationName, String crawlName) {
         super(object, parent, locationName, crawlName)
-        type = TYPE
+        type = this.getType()
         id = type + ':' + object.guid
+        guid = object.guid
+        ffId = object.id
+        idx = object.index
         name = object.title
 
         long lm = object.lastModified
@@ -49,6 +57,17 @@ class FFObject extends SavableObject {
             log.warn "No dateAdded date for FireFox object: ($this)"
         }
 
+        // todo -- save FF type to metadata??
+        // metadata.fftype = object.type //???
+    }
+
+    def walktree(def closure, int depth = 0) {
+        log.info "$depth) walk tree, this folder:$this (closure...)"
+        childGroups.each { FFFolder cf ->
+            closure(cf, depth)
+            def rs = cf.walktree(closure, depth++)
+            log.debug "$depth) result state: $rs"
+        }
     }
 
     /**
@@ -76,6 +95,29 @@ class FFObject extends SavableObject {
             }
         }
         return date
+    }
+
+
+    @Override
+    SolrInputDocument toPersistenceDocument() {
+        SolrInputDocument doc = super.toPersistenceDocument()
+        if (ffId) {
+            doc.addField('ffId_s', ffId)
+        } else {
+            log.warn "no FF ID ($ffId) for this object/bookmark:($this)"
+        }
+        if (guid) {
+            doc.addField('ffGuid_s', guid)
+        } else {
+            log.warn "no FF GUID ($guid) for this object/bookmark:($this)"
+        }
+        if (idx!=null) {
+            doc.addField('ffIndex_i', idx)
+        } else {
+            log.info "no FF ID (?null??) for this object/bookmark:($this)"
+        }
+
+        return doc
     }
 
 //    @Override
